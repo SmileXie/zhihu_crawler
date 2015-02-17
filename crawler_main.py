7d931fc9e3382402d618914b31840e77cf439e77
Smile <smilexie1113@gmail.com>
2015-02-17 22:07:00 +0800

    Obtain on web

--- crawler_main.py
+++ crawler_main.py
@@ -1,6 +1,19 @@
 import urllib.request
+import os
+import sys
+
+"""crawler study code """
  
 url = "http://www.baidu.com"
 data = urllib.request.urlopen(url).read()
-data = data.decode('UTF-8')
-print(data)
+web_str = data.decode("utf-8")
+
+try:
+    if not os.path.exists("tmp"):
+        os.mkdir("tmp")
+    local_file = open("tmp/local.htm", "w", encoding='utf-8')
+    local_file.write(web_str)
+    local_file.close()
+except IOError as e:
+    print("Could not open file:", e)
+
