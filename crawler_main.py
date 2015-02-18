0694c0db974d6af6d271e90b3a5c77bb9b8a8851
Smile <smilexie1113@gmail.com>
2015-02-18 19:10:18 +0800

    Grab a search result

--- crawler_main.py
+++ crawler_main.py
@@ -3,17 +3,22 @@ import os
 import sys
 
 """crawler study code """
- 
-url = "http://www.baidu.com"
-data = urllib.request.urlopen(url).read()
-web_str = data.decode("utf-8")
 
-try:
-    if not os.path.exists("tmp"):
-        os.mkdir("tmp")
-    local_file = open("tmp/local.htm", "w", encoding='utf-8')
-    local_file.write(web_str)
-    local_file.close()
-except IOError as e:
-    print("Could not open file:", e)
+if __name__ == "__main__":
+    search_info = {}
+    search_info["word"] = "2015羊年"
+    search_str = urllib.parse.urlencode(search_info)
+    url_pre = "http://www.baidu.com/s?"
+    url_full = url_pre + search_str
+    data = urllib.request.urlopen(url_full).read()
+    web_str = data.decode("utf-8")
+    
+    try:
+        if not os.path.exists("tmp"):
+            os.mkdir("tmp")
+        with open("tmp/local.htm", "w", encoding='utf-8') as local_file:
+            local_file.write(web_str)
+            
+    except IOError as e:
+        print("Could not open file:", e)
 
