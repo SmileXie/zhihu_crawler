a8b8f5d5521e0a13348c063f7816891182cf622e
Smile <smilexie1113@gmail.com>
2015-04-08 22:59:53 +0800

    use Request and BeautifulSoup to login zhihu
    Not finished

--- crawler_main.py
+++ crawler_main.py
@@ -7,21 +7,37 @@ Author: smilexie1113@gmail.com
 import requests
 from bs4 import BeautifulSoup
 
-
-
-
-
+def main():
+    url = r"http://www.zhihu.com/"
+    id = r"xxxxxxxxxx"
+    password = r"xxxxxxxxxx"
     
-    try:
-        html_str = urlfd.read().decode("utf-8")
-    except:
-        return ERROR_RETURN + ("Fail to decode URL " + "\"" + url + "\"" + ".")
+    header = {
+        'Connection': 'Keep-Alive',
+        'Accept': 'text/html, application/xhtml+xml, */*',
+        'Accept-Language': 'en-US,en;q=0.8,zh-Hans-CN;q=0.5,zh-Hans;q=0.3',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko',
+        'Accept-Encoding': 'gzip, deflate',
+        'Host': 'www.zhihu.com',
+        'DNT': '1'
+    }
     
-    return html_str
-
-
+    response = requests.post(url, headers = header)
+    text = response.text
+    soup = BeautifulSoup(text);
+    input_tag = soup.find("input", {"name": "_xsrf"})
+    xsrf = input_tag["value"]
+    
+    url += r"login"
+    post_dict = {
+        'rememberme': 'y',
+        'password': password,
+        'email': id,
+        '_xsrf':xsrf    
+    }
+    
+    reponse2 = requests.post(url, headers = header, data = post_dict)
+    pass
+    
 if __name__ == "__main__":
-    start_url = "http://news.dbanotes.net/"
-    
-    
-    
+    main()
