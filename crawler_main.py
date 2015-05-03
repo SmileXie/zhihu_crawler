2690020127fedce7b81ae675e0ca3a0ee570b222
Smile <smilexie1113@gmail.com>
2015-05-03 16:34:12 +0800

    Get the page after login successfully
    Get the page after login successfully

--- crawler_main.py
+++ crawler_main.py
@@ -7,37 +7,50 @@ Author: smilexie1113@gmail.com
 import requests
 from bs4 import BeautifulSoup
 
-def main():
-    url = r"http://www.zhihu.com/"
-    id = r"xxxxxxxxxx"
-    password = r"xxxxxxxxxx"
-    
+class ZhihuInspect(object):
     header = {
         'Connection': 'Keep-Alive',
         'Accept': 'text/html, application/xhtml+xml, */*',
-        'Accept-Language': 'en-US,en;q=0.8,zh-Hans-CN;q=0.5,zh-Hans;q=0.3',
+        'Accept-Language': 'zh-CN,zh;q=0.8',
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko',
-        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Encoding': 'gzip,deflate,sdch',
         'Host': 'www.zhihu.com',
         'DNT': '1'
     }
+    url = r"http://www.zhihu.com/"
+    id = r"xxxxx"
+    password = r"xxxxx"
     
-    response = requests.post(url, headers = header)
-    text = response.text
-    soup = BeautifulSoup(text);
-    input_tag = soup.find("input", {"name": "_xsrf"})
-    xsrf = input_tag["value"]
+    def __init__(self):
+        pass
     
-    url += r"login"
-    post_dict = {
-        'rememberme': 'y',
-        'password': password,
-        'email': id,
-        '_xsrf':xsrf    
-    }
+    def save_file(self, path, str):
+        with open(path, 'w') as fp:
+            fp.write(str)
     
-    reponse2 = requests.post(url, headers = header, data = post_dict)
-    pass
+    def get_xsrf(self):
+        response = requests.get(self.url, headers = self.header)
+        text = response.text
+        save_file("1.htm", text)
+        soup = BeautifulSoup(text);
+        input_tag = soup.find("input", {"name": "_xsrf"})
+        xsrf = input_tag["value"]
+        return xsrf
+        
+    def get_login_page(self, xsrf):
+        login_url = self.url + r"login"
+        post_dict = {
+            'rememberme': 'y',
+            'password': self.password,
+            'email': self.id,
+            '_xsrf':xsrf    
+        }
+        reponse_login = requests.post(login_url, headers = self.header, data = post_dict)
+        save_file('2.htm', reponse_login.text)
     
 if __name__ == "__main__":
-    main()
+    z = ZhihuInspect()
+    xsrf = z.get_xsrf()
+    z.get_login_page(xsrf)
+    
+    
